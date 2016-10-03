1f7874f6f10fd3347b70e341e18f11bb2366ed62
Hauxii <haukur91@gmail.com>
2016-10-03 10:32:40 +0000

    videoservice

--- src/main/java/is/ru/honn/rutube/service/VideoServiceStub.java
+++ src/main/java/is/ru/honn/rutube/service/VideoServiceStub.java
@@ -1,7 +1,21 @@
 package is.ru.honn.rutube.service;
 
+import is.ru.honn.rutube.domain.Video;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Lenny on 29.9.2016.
  */
 public class VideoServiceStub {
+    Video getVideo(int videoId){
+        return new Video(1, "bla","desc", "src", "type", new ArrayList<>());
+    }
+    List<Video> getVideosbyUser(int userId){
+        return new ArrayList<>();
+    }
+    int addVideo(Video video, int userId) throws ServiceException{
+        return 1;
+    }
 }
---
+++ src/test/java/is/ru/honn/rutube/service/VideoServiceStubTest.java
@@ -0,0 +1,32 @@
+package is.ru.honn.rutube.service;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Lenny on 3.10.2016.
+ */
+public class VideoServiceStubTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void getVideo() throws Exception {
+
+    }
+
+    @Test
+    public void getVideosbyUser() throws Exception {
+
+    }
+
+    @Test
+    public void addVideo() throws Exception {
+
+    }
+
+}
