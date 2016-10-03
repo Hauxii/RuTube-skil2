25c8426e1a40c5a648e9e0f4c90cc5806960dcf6
Hauxii <haukur91@gmail.com>
2016-10-03 13:50:35 +0000

    removed public from interface

--- src/main/java/is/ru/honn/rutube/reader/Reader.java
+++ src/main/java/is/ru/honn/rutube/reader/Reader.java
@@ -4,9 +4,9 @@ package is.ru.honn.rutube.reader;
  * Created by Berglind on 10/3/2016.
  */
 public interface Reader {
-    public Object read();
-    public Object parse(String content);
-    public void setURI(String URI);
-    public void setReadHandler(ReadHandler readHandler);
+    Object read();
+    Object parse(String content);
+    void setURI(String URI);
+    void setReadHandler(ReadHandler readHandler);
 
 }
