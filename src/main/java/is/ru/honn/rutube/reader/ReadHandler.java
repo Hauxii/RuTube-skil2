1d86d1c87696bd7ce7c9cf80c1ed9a2d51dc763c
berglindoma13 <berglindoma13@ru.is>
2016-10-03 13:23:46 +0000

    readers

--- src/main/java/is/ru/honn/rutube/reader/ReadHandler.java
+++ src/main/java/is/ru/honn/rutube/reader/ReadHandler.java
@@ -1,8 +1,9 @@
 package is.ru.honn.rutube.reader;
 
+
 /**
  * Created by Berglind on 10/3/2016.
  */
-public abstract class AbstractReader {
-    public AbstractReader(){}
+public class ReadHandler {
+    public ReadHandler(){}
 }
--- src/main/java/is/ru/honn/rutube/reader/Reader.java
+++ src/main/java/is/ru/honn/rutube/reader/Reader.java
@@ -7,6 +7,6 @@ public interface Reader {
     public Object read();
     public Object parse(String content);
     public void setURI(String URI);
-    //public void setReadHandler(ReadHandler readHandler);
+    public void setReadHandler(ReadHandler readHandler);
 
 }
