42369791a4adef526e82e13fae99a94e02c1d4ed
Hauxii <haukur91@gmail.com>
2016-10-04 10:25:41 +0000

    spring shit

--- src/main/java/is/ru/honn/rutube/reader/ReaderFactory.java
+++ src/main/java/is/ru/honn/rutube/reader/ReaderFactory.java
@@ -1,7 +1,19 @@
 package is.ru.honn.rutube.reader;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by Berglind on 10/3/2016.
  */
+@Configuration
 public class ReaderFactory {
+    ApplicationContext context = new ClassPathXmlApplicationContext("/reader.xml");
+
+    @Bean
+    public Object getReader(String reader){
+        return context.getBean(reader);
+    }
 }
